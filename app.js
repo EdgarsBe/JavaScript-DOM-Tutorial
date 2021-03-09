f79ee92ac84fba808d09bac34153e30ca66c58f6
EdgarsBe <edgarsberzins100@gmail.com>
2021-03-09 16:50:33 +0200

    12_lesson

--- app.js
+++ app.js
@@ -14,5 +14,21 @@ const addForm = document.forms['add-book'];
 addForm.addEventListener('submit', function(e){
     e.preventDefault();
     const value = addForm.querySelector('input[type="text"]').value;
-    console.log(value);
-})
+    
+    //creat element
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    //add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+
+
+    //append to document
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+
+
+});
